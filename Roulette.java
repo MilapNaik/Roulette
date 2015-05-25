f1777856faf77ec888f697d68833abe2af394849
Milap Naik <milapnaik@gmail.com>
2015-05-24 21:11:03 -0400

    Cleaned up code

--- Roulette.java
+++ Roulette.java
@@ -3,115 +3,95 @@
 public class Roulette
 {
 
-   public String[] value;
-   public String[] color;
+	public String[] value;
+	public String[] color;
+	public int outcome;
 
-   public int outcome;
-
-   /* ================================================
+    /*================================================
       Task 1: write the constructor
-      ================================================ */
-   public Roulette( )
-   { 
-     value = new String[38];
-     color = new String[38];
-     int i;
+      ================================================*/
+	public Roulette( )
+	{ 
+		value = new String[38];
+		color = new String[38];
+		int i;
      
-     for (i = 0; i< 37; i++)
-     {
-       value[i] = Integer.toString(i);
-     }
-     value[37] = "00";
-     for (i = 1; i<10; i=i+2)
-     {
-       color[i] = "R";
-     }
-     for (i = 2; i<11; i=i+2)
-     {
-       color[i] = "B";
-     }
-     for (i = 11; i<18; i=i+2)
-     {
-       color[i] = "B";
-     }
-     for (i = 12; i<19; i=i+2)
-     {
-       color[i] = "R";
-     }
-     for (i = 20; i<29; i=i+2)
-     {
-       color[i] = "B";
-     }
-     for (i = 19; i<28; i=i+2)
-     {
-       color[i] = "R";
-     }     
-     for (i = 30; i<37; i=i+2)
-     {
-       color[i] = "R";
-     }
-     for (i = 29; i<36; i=i+2)
-     {
-       color[i] = "B";
-     }
-     color[37] = "G";
-     color[0] = "G";
+		for (i = 0; i< 37; i++)
+			value[i] = Integer.toString(i);
+			
+		value[37] = "00";
+		for (i = 1; i<10; i=i+2)     
+			color[i] = "R";
      
+		for (i = 2; i<11; i=i+2)     
+			color[i] = "B";
+     
+		for (i = 11; i<18; i=i+2)     
+			color[i] = "B";
+     
+		for (i = 12; i<19; i=i+2)     
+			color[i] = "R";
+     
+		for (i = 20; i<29; i=i+2)     
+			color[i] = "B";
+     
+		for (i = 19; i<28; i=i+2)     
+			color[i] = "R";
+          
+		for (i = 30; i<37; i=i+2)
+			color[i] = "R";
+     
+		for (i = 29; i<36; i=i+2)     
+			color[i] = "B";
+     
+		color[37] = "G";
+		color[0] = "G";
+	}
 
 
-   }
-
-
-   /* ================================================
+	/*================================================
       Task 2: write the spin() method
-      ================================================ */
-   public void spin()
-   {
-     outcome = (int)(Math.random() * (38)); 
-     
-     
+      ================================================*/
+	public void spin()
+	{
+	   outcome = (int)(Math.random() * (38)); 
+	}
 
-   }
-
-
-   /* ************************************************
-      Task 2b: change the instance variable to  
+	/* ************************************************
+       Task 2b: change the instance variable to  
 
                    private
 
-      and recompile Test1.java and Test2.java
+       and recompile Test1.java and Test2.java
 
-      You should get errors
-      ************************************************ */
+       You should get errors
+       ************************************************ */
 
-
-
-
-   /* ================================================
+	/*================================================
       Task 3: write the value() method
-      ================================================ */
-   public String value()
-   {
-     String value1 = value[outcome];
-      return value1;
-   }
+      ================================================*/
+	public String value()
+	{
+		String valueoutcome = value[outcome];
+		return valueoutcome;
+	}
 
-   /* ================================================
+	/*================================================
       Task 4: write the color() method
-      ================================================ */
-   public String color()
-   {
-    String color1 = color[outcome];
-      return color1;
-   }
+      ================================================*/
+	public String color()
+	{
+		String color1 = color[outcome];
+		return color1;
+	}
 
-   /* ================================================
+	/*================================================
       Task 5: write the toString() method
-      ================================================ */
-     public String toString()   // Write this toString method....
-   {
-      return value[outcome] + " " + color[outcome];                   
-   }
+      ================================================*/
+	public String toString()   // Write this toString method....
+	{
+		return value[outcome] + " " + color[outcome];                   
+	}
 }
 
 
